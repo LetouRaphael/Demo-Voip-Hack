1c9d9992b7e88c3203dd8fcc5b413959c6de78e7
LetouRaphael <48223543+LetouRaphael@users.noreply.github.com>
2020-02-05 08:41:09 +0100

    Update sysupdate.sh

--- voiphack/conf/sysupdate.sh
+++ voiphack/conf/sysupdate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ $1 == "bad" ];then
-	scp /var/voiphack/conf/sip.conf.bad root@192.168.100.100:/etc/asterisk/sip.conf
-	ssh -t root@192.168.100.100 'asterisk -rx "sip reload"'
+if [ $1 == "bad" ];then			#Si commande exécuté "sysupdate.sh bad" alors installer configuration faillible 
+	scp /var/voiphack/conf/sip.conf.bad root@192.168.100.100:/etc/asterisk/sip.conf	#Envoi le fichierde configuation 
+	ssh -t root@192.168.100.100 'asterisk -rx "sip reload"'		#Envoi la commande de rechargement SIP sur le serveur en SSH
 	echo "allowguest=yes (Default) => Autorise la réponse aux périphérique non authentifié"
 	echo "call-limit= none (Default) => Autorise plusieurs appareil à se connecter en simultané avec les mêmes identifiants"
 	
@@ -14,8 +14,9 @@ elif [ $1 == "correct" ];then
 	
 	echo "allowguest=no => Interdit la réponse aux périphérique non authentifié"
 	echo "call-limit=1 => Autorise uniquement un appareil à la fois à utiliser un couple login:pass"
+	echo "alwaysauthreject=yes => Rejette les bruteforce"
 	echo "Complexification des mots de passe"
-	
+
 	echo "Success système corrigé en place"
 	
 
