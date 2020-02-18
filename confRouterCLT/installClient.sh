c4ff51c8cd942fdadbdae1e6165acaeae20e0ba5
LetouRaphael <48223543+LetouRaphael@users.noreply.github.com>
2020-02-18 09:14:18 +0100

    Add files via upload

---
+++ confRouterCLT/installClient.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+##### EN DEV ####
+
+cp interfaces /etc/network/interfaces
+cp dhcpd.conf /etc/dhcp/dhcpd.conf
+cp isc-dhcp-server /etc/default/isc-dhcp-server
+
+
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+
+sysctl -p
+
+apt-get install -y openvpn isc-dhcp-server
+
+echo "dev tap" > /etc/openvpn/configvpn.conf
+echo "remote 192.168.103.39" >> /etc/openvpn/configvpn.conf
+
+iptables -t nat -A POSTROUTING -o tap0 -j MASQUERADE
+iptables-save > /etc/iptables_rules.save
+
+
+reboot
